cb9e7d7fdcd45e2210b370de79bc959008755560
Nickolas Nikolic <subclass@gmail.com>
2016-07-16 14:06:42 -0500

    first upload... eeeeeewwwwwweeee

---
+++ app/js/services.js
@@ -0,0 +1,10 @@
+nonoApp
+  .factory( 'LoverRegistryService', function(){
+      var registry = {};
+
+      registry.userEmail = '';
+      registry.userId = '';
+      registry.userDate = {};
+
+      return registry;
+    });
