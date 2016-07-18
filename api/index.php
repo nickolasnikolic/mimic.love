33e2815431333c535a8d0f9275a96c8fa935372d
Nickolas Nikolic <nickolasnikolic@gmail.com>
2016-07-18 09:20:09 -0500

    testing

--- api/index.php
+++ api/index.php
@@ -6,13 +6,13 @@ error_reporting(-1);//tell me stuff
 $app = new \Slim\Slim();
 
 $app->post('/message', function(){
-    # First, instantiate the SDK with your API credentials and define your domain.
+    //First, instantiate the SDK with your API credentials and define your domain.
     $mg = new Mailgun(getenv('MAILGUN_API_KEY'));
     $domain = getenv('MAILGUN_DOMAIN');
 
     $message = implode('\n',$_POST);
 
-# Now, compose and send your message.
+    //Now, compose and send your message.
     $mg->sendMessage($domain, array(
         'from'    => 'info@mimic.love',
         'to'      => 'nick@mimic.love',
