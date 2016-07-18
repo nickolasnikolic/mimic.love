a020e7b4a67d6da25bcb5d4478f53bbf4dec53f1
Nickolas Nikolic <nickolasnikolic@gmail.com>
2016-07-18 09:37:07 -0500

    testing

--- api/index.php
+++ api/index.php
@@ -10,6 +10,8 @@ $app->post('/message', function(){
     $mg = new Mailgun(getenv('MAILGUN_API_KEY'));
     $domain = getenv('MAILGUN_DOMAIN');
 
+    print_r($_POST);
+
     $message = implode('\n',$_POST);
 
     //Now, compose and send your message.
